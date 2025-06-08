2222054a14c3834424775e2de4226a8352c64e85
wesley-sws <wesley-sws@users.noreply.github.com>
2025-06-08 02:22:54 +0100

    include convenience imports

---
+++ poker_engine/__init__.py
@@ -0,0 +1,10 @@
+"""WSPokerEngine - A comprehensive Texas Hold'em poker engine"""
+
+from .poker_manager import PokerManager
+from .hand_manager import HandManager
+from .players import Player
+from .cards import Card
+from .evaluate_hand import HandRank
+
+__version__ = "0.1.0"
+__all__ = ["PokerManager", "HandManager", "Player", "Card", "HandRank"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     python_requires=">=3.8",
-    author="Wesley Chan",
+    author="Wesley Sze",
     description="A comprehensive Texas Hold'em poker engine",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
